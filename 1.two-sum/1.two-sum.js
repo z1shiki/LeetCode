d90ce21e6f39a57f500bbe06ed608d4d39acf032
Z1shiki <z1stage@gmail.com>
2018-01-10 07:45:13 +0800

    two-sum

---
+++ 1.two-sum/1.two-sum.js
@@ -0,0 +1,53 @@
+/*
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (36.63%)
+ * Total Accepted:    745.6K
+ * Total Submissions: 2M
+ * Testcase Example:  '[3,2,4]\n6'
+ *
+ * Given an array of integers, return indices of the two numbers such that they
+ * add up to a specific target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * 
+ * Example:
+ * 
+ * Given nums = [2, 7, 11, 15], target = 9,
+ * 
+ * Because nums[0] + nums[1] = 2 + 7 = 9,
+ * return [0, 1].
+ * 
+ * 
+ */
+/**
+ * @param {number[]} numsX
+ * @param {number} target
+ * @return {number[]}
+ */
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const twoSum = function(nums, target) {
+    "use strict"
+    let set = new Set()
+    let output =[]
+    nums.every((item,index,array)=> {
+        if (set.has(item)) {
+            output=[index,nums.indexOf(target - item)].sort()
+            return false
+        }
+        set.add(target - item)
+        return true
+    });
+    return output
+};
+
+twoSum([8,2,5,4,6],8)

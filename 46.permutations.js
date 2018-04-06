5ac0787e165f4dee5cb9f8b706f61678ea0520dc
Z1shiki <z1stage@gmail.com>
2018-04-07 00:14:44 +0800

    a thinking way

---
+++ 46.permutations.js
@@ -0,0 +1,37 @@
+/*
+ * [46] Permutations
+ *
+ * https://leetcode.com/problems/permutations/description/
+ *
+ * algorithms
+ * Medium (45.95%)
+ * Total Accepted:    229.7K
+ * Total Submissions: 486.4K
+ * Testcase Example:  '[1,2,3]'
+ *
+ * 
+ * Given a collection of distinct numbers, return all possible permutations.
+ * 
+ * 
+ * 
+ * For example,
+ * [1,2,3] have the following permutations:
+ * 
+ * [
+ * ⁠ [1,2,3],
+ * ⁠ [1,3,2],
+ * ⁠ [2,1,3],
+ * ⁠ [2,3,1],
+ * ⁠ [3,1,2],
+ * ⁠ [3,2,1]
+ * ]
+ * 
+ * 
+ */
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var permute = function(nums) {
+    
+};
